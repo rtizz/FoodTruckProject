d28417c8eeff8ffe645c0a990696d4879f464e69
rob.tisdale <rtisdale.bt@gmail.com>
2023-01-22 10:44:52 -0600

    Final commit - Program working & README complete

--- README.md
+++ README.md
@@ -13,9 +13,10 @@ The overall purpose of this program is to allow the user to input up to 5 Food T
    ***NOTE*** If "quit" is entered on first prompt. No food trucks will be entered.
 3. Once at the main menu, the user will be prompted with 4 options:
 
+EXPECTED OUTCOMES <br>
     1. List all food trucks. - Lists out all food trucks entered with all accompanying data plus an automatically assigned Vendor ID.
     2. See average rating of food trucks. - Compiles all respective ratings and produces the average of all the food truck ratings.
-    3. Display highest-rated food truck. - Lists out the winning food truck will all respective data. 
+    3. Display highest-rated food truck. - Lists out the winning food truck will all respective data. If a tie, the program will print both highest rated.
     ***NOTE*** If more than one values share a high score, the program will default to the newest.
     4. Quit the program. - Self explanatory
 
@@ -23,7 +24,7 @@ The overall purpose of this program is to allow the user to input up to 5 Food T
 Additional (detailed) comments are left along side the code as well
 ## Foodtruck.java ##
 
-Lines:\t	Description <br> 
+Lines->Description <br> 
 4-8: 	Respective varable declaration and assignment(for one).<br>
 10-13 NoArg Ctor (not utilized in code).<br>
 15-21	Main Foodtruck Ctor taking in the key parameters. <br>
@@ -40,17 +41,17 @@ Lines:\t	Description <br>
 		15: Instatiating FoodtruckApp object to be used in methods below.<br>
 		16: Declaring and assigning a value to while loop boolean<br>
 		18-21: Primary two methods being utilized, one being looped.<br>
-26-50 populateFTArry method <br>
+28-55 populateFTArry method <br>
 		1. Prompts users <br>
 		2. Assigns input to variable <br>
 		   Breaks for loop if criteria is met to initiate while loop<br>
 		3. Sets those variables as parameters/arguments in Foodtruck Ctor<br>
-53-124menuList method - returning a boolean to kill the while loop if criteria met.<br>
+58-126menuList method - returning a boolean to kill the while loop if criteria met.<br>
 		Once user enters "quit" after entering 1 OR enter in 5 food trucks, populatFTArray method ends and menuList is initiated<br>
 		54-59: 4 option main menu with a scanner input to assign to switch statment following.<br>
 		61-122: Switch statement outlining the criteria for selecting items 1-4<br>
 		>>>>>>Case 4: Exiting the menu and ending the program sends the false boolean value out to the main method to kill the 
-				while loop.
+				while loop.<br>
 
 
 # Technologies Used
@@ -66,3 +67,4 @@ calling the information from.
 3. If technology can make your life easier, use it. I spent a while working with creating a toString in the wrong class before I realized eclipse does it for me where needed. 
 4. Get better at understanding my own flow and pausing at a good point. I might think I was at a good stopping point but my left over notes didnt give me a clear understanding of what I was doing when I came back.
 5. Better understand how to call tostrings. I feel I made Case 3 (menuList method) too convoluted when there was a much simpler way.
+6. Decimal Format is a very helpful tool when you cant cleanly get rid of .66666666666, .3333333333, etc with conventional methods. 
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 import com.skilldistillery.foodtruck.entities.Foodtruck;
@@ -9,23 +10,21 @@ public class FoodTruckApp {
 
 	private Foodtruck[] foodtrucksArray = new Foodtruck[5];
 	private Scanner scan = new Scanner(System.in);
-
+//main method
 	public static void main(String[] args) {
 		FoodTruckApp ftApp = new FoodTruckApp();
 
 		boolean isTrue = true;
 
-		ftApp.populateFTArray();
+		ftApp.populateFTArray(); //once complete: automatically starts the while loop for the main menu
 		while (isTrue) {
-		isTrue = ftApp.menuList();
-	
+		isTrue = ftApp.menuList();	
 		}
 
 
 	}
 	
-	
-	// makes new inputs to assign to Foodtruck ctor.
+// Prompts user and allows input to variables for Foodtruck Ctor
 	public void populateFTArray() {
 
 		// for loop
@@ -36,13 +35,15 @@ public class FoodTruckApp {
 			int rating;
 			System.out.println("Please enter the name of a foodtruck");
 			truckName = scan.nextLine();
-			if (truckName.equalsIgnoreCase("quit")) {
+//During this prompt only user can enter 'quit' for next menu
+			if (truckName.equalsIgnoreCase("quit")) { 
 				break;
 			} else {
 				System.out.println("Please enter the food truck cuisine");
 				foodType = scan.nextLine();
 				System.out.println("Please enter a star rating from 1 - 5 ");
 				rating = scan.nextInt();
+//making sure the program only accepts values 1-5
 				if (rating > 5 || rating < 1) {
 					System.err.println("Invalid Entry: Please select from 1 - 5");
 					rating = scan.nextInt();
@@ -53,30 +54,26 @@ public class FoodTruckApp {
 		}
 	}
 		
-
-
-
+//Menu list to see all compiled data. Returning a boolean to stop the while loop if criteria met
 	public boolean menuList() {
 		System.out.println("Please select the options below by entering the respective number");
 		System.out.println();
 		System.out.println(
-				" 1. List all food trucks. \n 2. See average rating of food trucks. \n 3. Display highest-rated food truck. \n 4. Quit the program. ");
+				" 1. List all food trucks. \n 2. See average rating of food trucks. \n 3. Display winning(highest-rated) food truck. \n 4. Quit the program. ");
 
 		int numSelect = scan.nextInt();
 
 		switch (numSelect) {
-		case 1:
+		case 1: //Displays all foodtrucks entered
 			System.out.println("Foodtrucks: ");
 			for (int i = 0; i < foodtrucksArray.length; i++) {
 				if (foodtrucksArray[i] != null) {
-					System.out.println(foodtrucksArray[i]);
+					System.out.println(foodtrucksArray[i]); //calls on toString from FoodTruck class
 					
 				}
 			}
-//			System.out.println("...Returning to menu...");
-//			menuList();
 			break;
-		case 2:
+		case 2://Displays the average rating of all entered trucks
 			double counter = 0;
 			double total = 0;
 			for (int numbOfTrucks = 0; numbOfTrucks < foodtrucksArray.length; numbOfTrucks ++) {
@@ -87,44 +84,39 @@ public class FoodTruckApp {
 				
 			}
 			double avg = total / counter;
-			//avg = Math.round(avg*) /2;
-			System.out.println("The average foodtruck rating was: " + avg + " stars.");
+			DecimalFormat df = new DecimalFormat("#.#"); //covers down on anything /3 that produces .66666/.33333/etc.
+			System.out.println("The average foodtruck rating was: " + df.format(avg) + " stars.");
 			break;
-		case 3:
-			String winnerName = null;
-			int winnerId = 0; 
-			String winnerFood = null;
-			int winnerRating = 0;
+		case 3: //Displays winning trucks information. 
+			int numbOfTrucks = 0;
 			int highestRatedNum = foodtrucksArray[0].getRating();
-			for(int numbOfTrucks = 0; numbOfTrucks < foodtrucksArray.length; numbOfTrucks ++) {
+			//loop to find the highest score
+			for(numbOfTrucks = 0; numbOfTrucks < foodtrucksArray.length; numbOfTrucks ++) {
 				if (foodtrucksArray[numbOfTrucks] != null) {
-					if (foodtrucksArray[numbOfTrucks].getRating() > highestRatedNum) {
-						//highestRatedNum = foodtrucksArray[numbOfTrucks].getRating();
-						
-						winnerName = foodtrucksArray[numbOfTrucks].getName();
-						winnerId = foodtrucksArray[numbOfTrucks].getId();
-						winnerFood = foodtrucksArray[numbOfTrucks].getFoodType();
-						winnerRating = foodtrucksArray[numbOfTrucks].getRating();
-						
-					} else if (foodtrucksArray[numbOfTrucks].getRating() == highestRatedNum) {
-						winnerName = foodtrucksArray[numbOfTrucks].getName();
-						winnerId = foodtrucksArray[numbOfTrucks].getId();
-						winnerFood = foodtrucksArray[numbOfTrucks].getFoodType();
-						winnerRating = foodtrucksArray[numbOfTrucks].getRating();
-					}
+					if (highestRatedNum < foodtrucksArray[numbOfTrucks].getRating()) {
+						highestRatedNum = foodtrucksArray[numbOfTrucks].getRating();
+					} 
 				}
-			} 
-			//System.out.println(highestRatedNum);
-			System.out.println(" The winning food truck was, " + winnerName + ", VendorID: " + winnerId +  ", Food-Type: " + winnerFood + ", Rating: " + winnerRating + "]");
-			break;
-		case 4:
+			}
+			//loop to correlate that score to respective truck
+			for(numbOfTrucks = 0; numbOfTrucks < foodtrucksArray.length; numbOfTrucks ++) {
+				if (foodtrucksArray[numbOfTrucks] != null) {
+					if (highestRatedNum == foodtrucksArray[numbOfTrucks].getRating()) {
+						System.out.println("The winning food truck was:\n" + foodtrucksArray[numbOfTrucks].toString());
+					} 
+				}
+			}
+			return true;
+//			break;
+		case 4: //Exits the program. and returns a false value to main method to stop while loop.
 			System.out.println("Exiting Program");
-			// isTrue = false;
+			
 			scan.close(); 
 			return false;
-		default:
+		default://Checks if numbers are between 1 and 4
 			if (numSelect != 1 || numSelect != 2 || numSelect != 3 || numSelect != 4) {
-			System.err.println("Invalid Entry: Please select 1-4");
+			System.err.println("Invalid Entry: Please select numbers 1-4.\nEnter any number and select enter to return to menu");
+			scan.nextLine();
 			numSelect = scan.nextInt();
 			}
 			break;
